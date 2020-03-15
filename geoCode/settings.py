1f795a929d1fe3d3eb4e0c5bab04eac09a3d8df9
Roshan Pandey <pandeyroshan556@gmail.com>
2020-03-16 02:13:23 +0530

    Update settings.py

--- geoCode/settings.py
+++ geoCode/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#bph#zp&o23=_u(f+i4f$++j$&a2*4mucaz!l95ai!#!llk0#b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['/']
 
 
 # Application definition
